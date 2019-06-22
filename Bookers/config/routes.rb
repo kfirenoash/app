f6ca3d10eba5ce0072eb1b7ee5caf066ddea3886
kfirenosh <kfirenoash@gmail.com>
2019-06-22 13:29:28 +0900

    [fix]

--- Bookers/config/routes.rb
+++ Bookers/config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   get 'books/show'
   get 'books' =>'books#index'
   get 'books/new'
-  get 'books/edit'
+  # get 'books/edit'
   root 'root#top'
   post 'books' => 'books#create'
   get 'books/:id' => 'books#show', as: 'post'
   get 'books/:id/edit' => 'books#edit', as: 'edit_post'
   patch 'books/:id' => 'books#update', as: 'update_post'
   delete 'books/:id' => 'books#destroy', as: 'destroy_post'
-  root "books#index"
+  # root "books#index"
 
   resources :books
 
